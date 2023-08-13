e13e099f3056103e6becaa8fbc455983236bb9f2
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-13 18:00:24 +0300

    Add: index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,50 @@
 <template>
-   <div class="container"></div>
+   <section class="relative bg-primary font-Giga overflow-hidden">
+      <div class="container pt-32 lg:pt-40 xl:pt-56">
+         <svg
+            class="absolute top-[-350px] left-[-340px] z-[-0] w-[700px] h-[700px] sm:top-[-300px] sm:left-[-300px] md:w-[800px] md:h-[800px]"
+            fill="none">
+            <use xlink:href="@/assets/img/sprite.svg#indexStain-1"></use>
+         </svg>
+         <div class="relative lg:flex">
+            <div>
+               <h1
+                  class="relative z-[1] block w-fit font-Beret text-[48px] leading-tight text-white font-bold text-center uppercase mx-auto mb-6 sm:text-[54px] md:text-[68px] md:leading-none lg:text-left lg:mx-0 lg:mb-8 2xl:text-[72px]">
+                  All you need <br />
+                  to glowy <br />
+                  skin & body
+                  <svg class="absolute -top-8 left-1/2 -translate-x-1/2 w-32 h-12 sm:w-36 sm:h-16 sm:-top-12 lg:translate-x-0 lg:left-11" fill="none">
+                     <use xlink:href="@/assets/img/sprite.svg#indexTitleIcon-1"></use>
+                  </svg>
+                  <svg class="absolute -top-16 -right-4 w-8 h-8 sm:-right-12 md:-right-20 lg:-right-14" fill="none">
+                     <use xlink:href="@/assets/img/sprite.svg#indexIcon-1"></use>
+                  </svg>
+               </h1>
+               <p class="relative z-[1]font-Giga text-white text-center leading-snug max-w-[350px] mx-auto mb-6 lg:mx-0 lg:text-left">
+                  Our natural and perfects products. We are here to show you that natural is perfect. <br />
+                  This is what our products are.
+               </p>
+               <NuxtLink
+                  class="flex justify-center items-center w-40 h-20 bg-white text-tertiary rounded-[55px] font-bold border border-tertiary hover:bg-secondary hover:text-white transition-all mx-auto mb-10 lg:mx-0 lg:mb-20 xl:mb-24"
+                  to="/">
+                  <span>SHOP NOW</span>
+               </NuxtLink>
+            </div>
+            <div class="relative">
+               <img
+                  class="relative block z-[1] w-[180%] h-auto max-w-[800px] translate-x-[-31%] md:translate-x-[-20%] lg:translate-x-0 lg:absolute lg:-left-40 lg:w-[800px] lg:-top-32 xl:-left-5 xl:-top-44 xl:w-[900px] xl:max-w-[900px] 2xl:left-10"
+                  src="@/assets/img/index__img-1.png"
+                  alt="picture"
+                  loading="lazy"
+                  width="200"
+                  height="200" />
+               <svg
+                  class="absolute bottom-[-280px] left-[-100px] z-0 w-[600px] h-[600px] sm:left-[-20px] sm:bottom-[-150px] md:left-[30px] md:w-[700px] md:h-[700px] md:bottom-[-300px] xl:left-40 2xl:left-64 2xl:bottom-[-250px]"
+                  fill="none">
+                  <use xlink:href="@/assets/img/sprite.svg#indexStain-2"></use>
+               </svg>
+            </div>
+         </div>
+      </div>
+   </section>
 </template>
