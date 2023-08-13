32e51172758cce09355626e45b73f156a3ddc2b9
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-13 12:58:51 +0300

    Add: nuxt img

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -47,7 +47,7 @@
                         <li class="overflow-hidden relative rounded-xl xl:rounded-3xl">
                            <NuxtLink class="relative" to="/">
                               <span class="absolute top-3 left-3 z-50 text-white font-Giga font-medium xl:top-5 xl:left-5 xl:text-lg">Cosmetics</span>
-                              <img
+                              <nuxt-img
                                  class="w-44 h-[140px] hover:scale-125 transition-all duration-300 object-cover xl:w-60 xl:h-48"
                                  src="~/assets/img/header__link-1.png"
                                  alt="picture"
