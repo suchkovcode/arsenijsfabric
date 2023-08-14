5c2d9832920a2c0b1a417ebd1c29b81ddf53c652
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-14 15:02:56 +0300

    Add: second section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-   <section class="relative bg-primary font-Giga overflow-hidden pt-20 md:pt-24 lg:pt-36 xl:44">
+   <section class="relative bg-primary font-Giga overflow-hidden pt-20 md:pt-24 lg:pt-36 xl:pt-44">
       <div class="container pt-32 lg:pt-40 xl:pt-52">
          <svg
             class="absolute top-[-350px] left-[-340px] z-[-0] w-[700px] h-[700px] sm:top-[-300px] sm:left-[-300px] md:w-[800px] md:h-[800px]"
@@ -45,6 +45,128 @@
                </svg>
             </div>
          </div>
+      </div>
+   </section>
+   <section class="relative bg-violet py-12 lg:py-14 xl:py-16">
+      <div class="container">
+         <div
+            class="grid grid-cols-2 items-center justify-items-center gap-6 max-w-[400px] mx-auto py-10 lg:py-12 xl:py-13 md:grid-cols-4 md:max-w-none">
+            <div class="inline-flex flex-col items-center gap-2 w-28 lg:flex-row lg:w-auto lg:gap-3">
+               <svg class="w-12 h-12" fill="none">
+                  <use xlink:href="@/assets/img/sprite.svg#iconProduct-1"></use>
+               </svg>
+               <span class="text-center lg:text-left leading-none">
+                  Natural Ingredients
+               </span>
+            </div>
+            <div class="inline-flex flex-col items-center gap-2 w-28 lg:flex-row lg:w-auto lg:gap-3">
+               <svg class="w-12 h-12" fill="none">
+                  <use xlink:href="@/assets/img/sprite.svg#iconProduct-2"></use>
+               </svg>
+               <span class="text-center lg:text-left leading-none">
+                  Cruelty free
+               </span>
+            </div>
+            <div class="inline-flex flex-col items-center gap-2 w-28 lg:flex-row lg:w-auto lg:gap-3">
+               <svg class="w-12 h-12" fill="none">
+                  <use xlink:href="@/assets/img/sprite.svg#iconProduct-3"></use>
+               </svg>
+               <span class="text-center lg:text-left leading-none">
+                  Handmade
+               </span>
+            </div>
+            <div class="inline-flex flex-col items-center gap-2 w-28 lg:flex-row lg:w-auto lg:gap-3">
+               <svg class="w-12 h-12" fill="none">
+                  <use xlink:href="@/assets/img/sprite.svg#iconProduct-4"></use>
+               </svg>
+               <span class="text-center lg:text-left leading-none">
+                  Vegan cosmetics
+               </span>
+            </div>
+         </div>
+         <nav>
+            <ul class="grid grid-cols-12 grid-rows-mini-2 gap-3 md:gap-4 xl:gap-5">
+               <li
+                  class="overflow-hidden col-span-7 relative h-[200px] rounded-3xl sm:h-[250px] md:h-[300px] lg:h-[355px] lg:col-span-5 lg:row-span-2 xl:h-[460px]">
+                  <NuxtLink class="relative" to="/">
+                     <span class="absolute top-3 left-3 z-50 text-white font-Giga eading-tight sm:top-5 sm:left-5 xl:top-5 xl:left-5 xl:text-lg">
+                        <span class="inline-block font-normal text-sm mb-[2px]">The</span> <br />
+                        <span class="font-Beret uppercase md:text-xl xl:text-2xl">Cosmetics</span>
+                     </span>
+                     <img
+                        class="w-full h-full hover:scale-125 transition-all duration-300 object-cover"
+                        src="@/assets/img//header__link-1.png"
+                        alt="picture"
+                        loading="lazy" />
+                     <svg class="absolute bottom-3 left-3 w-7 h-7 xl:bottom-5 xl:left-5" fill="none">
+                        <use xlink:href="@/assets/img/sprite.svg#iconArrow"></use>
+                     </svg>
+                  </NuxtLink>
+               </li>
+               <li class="overflow-hidden col-span-5 relative h-[200px] rounded-3xl sm:h-[250px] md:h-[300px] lg:h-full lg:col-span-3 lg:row-span-2">
+                  <NuxtLink class="relative" to="/">
+                     <span
+                        class="absolute top-3 left-3 z-50 w-[85%] text-white font-Giga leading-tight sm:top-5 sm:left-5 xl:top-5 xl:left-5 xl:text-lg">
+                        <span class="inline-block font-normal text-sm mb-[2px]">The</span> <br />
+                        <span class="font-Beret relative flex justify-between items-center uppercase md:text-xl xl:text-2xl">
+                           Soap bouquets
+                           <svg class="hidden w-6 h-6 md:block" fill="none">
+                              <use xlink:href="@/assets/img/sprite.svg#iconHand"></use>
+                           </svg>
+                        </span>
+                     </span>
+                     <img
+                        class="w-full h-full hover:scale-125 transition-all duration-300 object-cover"
+                        src="@/assets/img/product__item-2.png"
+                        alt="picture"
+                        loading="lazy" />
+                     <svg class="absolute bottom-3 left-3 w-7 h-7 xl:bottom-5 xl:left-5" fill="none">
+                        <use xlink:href="@/assets/img/sprite.svg#iconArrow"></use>
+                     </svg>
+                  </NuxtLink>
+               </li>
+               <li
+                  class="overflow-hidden col-span-6 relative h-[120px] rounded-3xl sm:h-[180px] md:h-[200px] lg:h-[170px] lg:col-span-4 lg:row-span-1 xl:h-[220px]">
+                  <NuxtLink class="relative" to="/">
+                     <span
+                        class="absolute top-3 left-3 z-50 w-[85%] text-white font-Giga leading-tight sm:top-5 sm:left-5 xl:top-5 xl:left-5 xl:text-lg">
+                        <span class="inline-block font-normal text-sm mb-[2px]">The</span> <br />
+                        <span class="font-Beret relative flex justify-between items-centeruppercase md:text-xl xl:text-2xl">
+                           Soap
+                           <svg class="hidden w-6 h-6 md:block" fill="none">
+                              <use xlink:href="@/assets/img/sprite.svg#iconHand"></use>
+                           </svg>
+                        </span>
+                     </span>
+                     <img
+                        class="w-full h-full hover:scale-125 transition-all duration-300 object-cover"
+                        src="@/assets/img/header__link-3.png"
+                        alt="picture"
+                        loading="lazy" />
+                     <svg class="absolute bottom-3 left-3 w-7 h-7 xl:bottom-5 xl:left-5" fill="none">
+                        <use xlink:href="@/assets/img/sprite.svg#iconArrow"></use>
+                     </svg>
+                  </NuxtLink>
+               </li>
+               <li
+                  class="overflow-hidden col-span-6 relative h-[120px] rounded-3xl sm:h-[180px] md:h-[200px] lg:h-[170px] lg:col-span-4 lg:row-span-1 xl:h-[220px]">
+                  <NuxtLink class="relative" to="/">
+                     <span class="absolute top-3 left-3 z-50 text-white font-Giga eading-tight sm:top-5 sm:left-5 xl:top-5 xl:left-5 xl:text-lg">
+                        <span class="inline-block font-normal text-sm mb-[2px]">The</span> <br />
+                        <span class="font-Beret uppercase md:text-xl xl:text-2xl">Bath bombs</span>
+                     </span>
+                     <img
+                        class="w-full h-full hover:scale-125 transition-all duration-30 object-cover 8"
+                        src="@/assets/img/header__link-4.png"
+                        alt="picture"
+                        loading="lazy" />
+                     <svg class="absolute bottom-3 left-3 w-7 h-7 xl:bottom-5 xl:left-5" fill="none">
+                        <use xlink:href="@/assets/img/sprite.svg#iconArrow"></use>
+                     </svg>
+                  </NuxtLink>
+               </li>
+            </ul>
+         </nav>
       </div>
    </section>
 </template>
