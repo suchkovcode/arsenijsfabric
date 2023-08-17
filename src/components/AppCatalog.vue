7f81af4d07e3cd882d5004fce2cd45209f95b8cf
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-17 20:46:59 +0300

    Add: catalog component

---
+++ src/components/AppCatalog.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="grid grid-cols-catalog justify-items-center gap-8">
+      <card v-for="item in dataItem" :key="item.id" :card-data="item"></card>
+   </div>
+</template>
+
+<script>
+export default {
+   props: {
+      dataItem: {
+         type: Array,
+         required: true,
+      },
+   },
+};
+</script>
