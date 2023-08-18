ca08cfb9ef2f965a682889b07e8518cdbee74bec
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-18 18:16:51 +0300

    Add: breadcrumbs component

---
+++ src/components/block/v-breadcrumbs.vue
@@ -0,0 +1,22 @@
+<template>
+   <nav class="mb-30">
+      <ul class="flex flex-wrap justify-center items-center lg:justify-start">
+         <li v-for="item in roterLink" :key="item.id" :class="{ active: item.last }" class="breadcrumbs__item cursor-pointer text-base">
+            <router-link class="breadcrumbs__link font-Giga text-tertiary text-sm" :to="item.to">
+               {{ item.name }}
+            </router-link>
+         </li>
+      </ul>
+   </nav>
+</template>
+
+<script>
+export default {
+   props: {
+      roterLink: {
+         type: Array,
+         required: true,
+      },
+   },
+};
+</script>
