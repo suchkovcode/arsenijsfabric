27ed9553b3b735488be813b6bb9307745168e30b
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-18 18:47:37 +0300

    Fix: valid favorite btn

--- src/components/card/favorite.vue
+++ src/components/card/favorite.vue
@@ -1,6 +1,8 @@
 <template>
    <button
       class="cursor-pointer absolute top-4 right-4 flex justify-center items-center w-11 h-11 border border-primary rounded-full bg-violet text-transparent hover:text-primary hover:fill-primary"
+      type="button"
+      aria-label="Favorite btn product card"
       @mouseover="isHover = true"
       @mouseleave="isHover = false">
       <svg class="w-6 h-6" :class="{ 'fill-primary': isHover }">
