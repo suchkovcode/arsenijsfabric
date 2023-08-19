bba5a388c59c40ab0c57be50a1c80ffa679c448f
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-19 15:19:26 +0300

    Add: empty product store

--- src/store/product.js
+++ src/store/product.js
@@ -2,7 +2,42 @@ import { defineStore } from "pinia";
 
 export const useProductStore = defineStore("product", {
    state: () => {
-      return {};
+      return {
+         products: [
+            {
+               id: 1,
+               name: "card-text",
+            },
+            {
+               id: 2,
+               name: "card-text",
+            },
+            {
+               id: 3,
+               name: "card-text",
+            },
+            {
+               id: 4,
+               name: "card-text",
+            },
+            {
+               id: 5,
+               name: "card-text",
+            },
+            {
+               id: 6,
+               name: "card-text",
+            },
+            {
+               id: 7,
+               name: "card-text",
+            },
+            {
+               id: 8,
+               name: "card-text",
+            },
+         ],
+      };
    },
 
    getters: {},
