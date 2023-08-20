618c2fafefd31b3bf643da48eb2ab29597115061
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-20 13:49:08 +0300

    Format

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -42,9 +42,9 @@
                         </svg>
                         Bestsellers
                      </p>
-                     <NuxtLink class="text-xs underline leading-none transition-all hover:text-secondary" to="/catalog" @click="shopHover = false"
-                        >ALL PRODUCTS</NuxtLink
-                     >
+                     <NuxtLink class="text-xs underline leading-none transition-all hover:text-secondary" to="/catalog" @click="shopHover = false">
+                        ALL PRODUCTS
+                     </NuxtLink>
                   </div>
                   <nav>
                      <ul class="flex gap-3 justify-end">
