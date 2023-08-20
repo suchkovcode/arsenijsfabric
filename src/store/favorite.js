5a8af1c7266bea6b8603a89144e4718e186fd7db
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-20 09:41:02 +0300

    Update: state

--- src/store/favorite.js
+++ src/store/favorite.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const useFavoriteStore = defineStore("favorite", {
    state: () => {
       return {
-         isActive: true,
+         isActive: false,
       };
    },
 
