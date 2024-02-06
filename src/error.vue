c20f1f33dad55cfee9d5b1125b3302095e161233
Nikita Suchkov <suchkov8520@gmail.com>
2024-02-06 15:49:55 +0200

    Update error page

--- src/error.vue
+++ src/error.vue
@@ -1,25 +1,21 @@
 <template>
-   <section class="flex justify-center items-center w-full h-screen bg-primary">
+   <section class="flex h-screen w-full items-center justify-center bg-primary">
       <div class="container">
-         <h1 class="mb-5 font-Beret text-[110px] text-center text-white">404</h1>
-         <p class="mb-8 text-center text-lg text-white font-Giga opacity-70">
+         <h1 class="mb-5 text-center font-Beret text-[110px] text-white">404</h1>
+         <p class="mb-8 text-center font-Giga text-lg text-white opacity-70">
             This page does not exist <br />
             or has it been removed
          </p>
          <router-link
-            class="flex justify-center items-center w-full h-11 max-w-[240px] mx-auto rounded-[55px] border border-tertiary font-Beret bg-secondary text-white tracking-wide uppercase hover:bg-white hover:text-secondary transition-all"
+            class="mx-auto flex h-11 w-full max-w-[240px] items-center justify-center rounded-[55px] border border-tertiary bg-secondary font-Beret uppercase tracking-wide text-white transition-all hover:bg-white hover:text-secondary"
             to="/">
             Go back
          </router-link>
       </div>
    </section>
 </template>
-<script>
-export default {
-   setup() {
-      useSeoMeta({
-         title: "Arsenijs Fabric | 404",
-      });
-   },
-};
+<script setup>
+useSeoMeta({
+   title: "Arsenijs Fabric | 404",
+});
 </script>
