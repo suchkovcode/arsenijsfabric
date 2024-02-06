c108b75b4e6175bbfff549d3d46e62aad9e8c9d1
Nikita Suchkov <suchkov8520@gmail.com>
2024-02-06 15:54:19 +0200

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,43 +1,30 @@
 <template>
+   <Head>
+      <Title>Arsenijs Fabric | Home</Title>
+      <Meta charset="utf-8" />
+      <Meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <Meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <Meta name="apple-mobile-web-app-title" content="Arsenijs Fabric | Home" />
+      <Meta name="apple-mobile-web-app-capable" content="yes" />
+      <Meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <Meta name="robots" content="noindex, nofollow" />
+      <Meta name="description" content="title" />
+      <Link rel="shortcut icon" type="image/png" href="/static/favicon.ico" />
+      <Link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+      <Link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+      <Link rel="icon" type="image/png" sizes="48x48" href="/static/favicon-48x48.png" />
+      <Link rel="icon" type="image/png" sizes="228x228" href="/static/coast-228x228.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="57x57" href="/static/apple-touch-icon-57x57.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="60x60" href="/static/apple-touch-icon-60x60.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="72x72" href="/static/apple-touch-icon-72x72.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="76x76" href="/static/apple-touch-icon-76x76.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="114x114" href="/static/apple-touch-icon-114x114.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="152x152" href="/static/apple-touch-icon-152x152.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="167x167" href="/static/apple-touch-icon-167x167.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/static/apple-touch-icon-180x180.png" />
+      <Link rel="apple-touch-icon" type="image/png" sizes="1024x1024" href="/static/apple-touch-icon-1024x1024.png" />
+   </Head>
    <NuxtLayout>
       <NuxtPage />
    </NuxtLayout>
 </template>
-
-<script>
-import ogImage from "~/assets/img/preview.png";
-export default {
-   setup() {
-      useHead({
-         htmlAttrs: {
-            lang: "en",
-         },
-         meta: [
-            { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
-            { name: "apple-mobile-web-app-title", content: "Arsenijs Fabric" },
-            { name: "apple-mobile-web-app-capable", content: "yes" },
-            { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
-            { hid: "og:title", property: "og:title", content: "Arsenijs Fabric | Home" },
-            { hid: "og:url", property: "og:url", content: "https://arsenijsfabric.net" },
-            { hid: "og:description", property: "og:description", content: "Our natural and perfects products. We are here to show you that natural is perfect. This is what our products are." },
-            { hid: "og:image", property: "og:image", content: ogImage },
-
-            { hid: "twitter:title", name: "twitter:title", content: "Arsenijs Fabric | Home" },
-            { hid: "twitter:url", name: "twitter:url", content: "https://arsenijsfabric.net" },
-            { hid: "twitter:description", name: "twitter:description", content: "Our natural and perfects products. We are here to show you that natural is perfect. This is what our products are." },
-            { hid: "twitter:image", name: "twitter:image", content: ogImage },
-         ],
-         link: [
-            { hid: "canonical", rel: "canonical", href: "https://arsenijsfabric.net" },
-         ],
-      });
-
-      useSeoMeta({
-         title: "Arsenijs Fabric | Home",
-         description: "Описание не длиннее 155 символов",
-      });
-
-      defineRobotMeta();
-   },
-};
-</script>
