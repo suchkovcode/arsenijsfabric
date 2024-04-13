3888b94610653d3564900eccad8722fdf1a22b5a
Nikita Suchkov <suchkov8520@gmail.com>
2024-04-13 18:06:46 +0300

    Update card

--- src/components/card/v-card.vue
+++ src/components/card/v-card.vue
@@ -2,21 +2,30 @@
    <article class="w-full max-w-[340px] overflow-hidden rounded-3xl">
       <header class="relative">
          <v-favorite />
-         <img class="block h-[230px] w-full object-cover md:h-[250px]" src="" alt="card picture" loading="lazy" width="400" height="400" />
+         <UnLazyImage
+            class="block h-[230px] w-full object-cover md:h-[250px]"
+            :blurhash="cardData.blurhash"
+            :src-set="cardData.img"
+            width="400"
+            height="400" />
       </header>
       <div class="flex flex-col gap-3 bg-violet p-5">
          <v-rating />
-         <h2 class="max-w-[230px] font-Beret text-lg leading-none text-tertiary">HOT SETTING ANTI AGING Alginate Face mask</h2>
-         <p class="mb-3 min-h-[30px] text-sm text-tertiary opacity-60">with Tea Tree Leaf oil; Sea Silt Extract and Burdock Root Extract</p>
+         <h2 class="max-w-[230px] font-Beret text-lg leading-none text-tertiary">
+            {{ cardData.title }}
+         </h2>
+         <p class="mb-3 h-[60px] min-h-[30px] overflow-hidden text-ellipsis text-sm text-tertiary opacity-60">
+            {{ cardData.previews }}
+         </p>
          <div class="flex items-center gap-2 font-Giga">
-            <p class="text-xl text-[#b4b3bd] line-through">9,95 €</p>
-            <p class="text-xl font-medium text-tertiary">5,95 €</p>
+            <p v-if="cardData.price.old" class="text-xl text-[#b4b3bd] line-through">{{ cardData.price.old }} €</p>
+            <p class="text-xl font-medium text-tertiary">{{ cardData.price.new }} €</p>
          </div>
       </div>
       <footer class="flex flex-col gap-3 bg-violet p-5">
          <NuxtLink
             class="flex h-11 w-full items-center justify-center rounded-[55px] border border-tertiary bg-secondary font-Beret tracking-wide text-white"
-            :to="`/catalog/${1}`">
+            :to="`/catalog/item-${cardData.id}`">
             Learn more
          </NuxtLink>
          <button
