db8a73d5974fde8bf03a558677f0d6d288bed684
Nikita Suchkov <suchkov8520@gmail.com>
2024-04-13 18:44:32 +0300

    Update card page component

--- src/pages/catalog/item-[id].vue
+++ src/pages/catalog/item-[id].vue
@@ -1,24 +1,18 @@
 <template>
    <section class="mt-16 lg:mt-24">
       <div class="container py-12">
-         <block-v-breadcrumbs class="mb-6" :roter-link="{ catalog: true, name: 'cards.title', to: $route.fullPath }" />
+         <v-breadcrumbs class="mb-6" :roter-link="{ catalog: true, name: 'cards.title', to: $route.fullPath }" />
          <article class="flex flex-col gap-5 lg:flex-row lg:gap-8">
-            <img
-               class="mx-auto w-full max-w-[460px] rounded-xl lg:mx-0"
-               src=""
-               alt="card picture"
-               loading="lazy"
-               width="400"
-               height="400" />
+            <img class="mx-auto w-full max-w-[460px] rounded-xl lg:mx-0" src="" alt="card picture" loading="lazy" width="400" height="400" />
             <div class="mx-auto max-w-[460px] lg:mx-0 lg:max-w-none">
-               <card-rating class="mb-3" :rating-storage="currentCardData" @rating-data="addRatingData" />
+               <card-rating class="mb-3" />
                <h2 class="mb-5 font-Beret text-2xl uppercase leading-none text-tertiary">HOT SETTING ANTI AGING Alginate Face mask</h2>
                <div class="mb-5 flex items-center gap-2 font-Giga">
                   <p class="text-xl text-[#b4b3bd] line-through">9,95 €</p>
                   <p class="text-xl font-medium text-tertiary">5,95 €</p>
                </div>
                <div class="relative mb-5 flex items-center gap-3">
-                  <card-count :count-data="1" />
+                  <v-count :count-data="1" @count-item="count = $event" />
                   <button
                      class="flex h-11 w-full max-w-[150px] items-center justify-center gap-2 rounded-[55px] border border-tertiary bg-primary font-Beret tracking-wide text-white">
                      <svg class="h-5 w-5">
@@ -26,7 +20,7 @@
                      </svg>
                      Buy now
                   </button>
-                  <card-favorite class="relative !left-0 !top-0" />
+                  <v-favorite class="relative !left-0 !top-0" />
                </div>
                <div>
                   <p class="mb-3 font-Giga text-sm text-tertiary">
@@ -48,6 +42,6 @@
 </template>
 
 <script setup>
+const count = ref();
 const currentCardData = ref({});
-const isHover = ref(false);
 </script>
