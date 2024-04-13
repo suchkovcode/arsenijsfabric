d08efa0a51a8b9c0ded2dff541e6e89d747d794b
Nikita Suchkov <suchkov8520@gmail.com>
2024-04-13 17:53:44 +0300

    Update catalog

---
+++ src/components/global/app-catalog.vue
@@ -0,0 +1,14 @@
+<template>
+   <div class="grid grid-cols-catalog justify-items-center gap-6">
+      <v-card v-for="item in dataItem" :key="item.id" :card-data="item" />
+   </div>
+</template>
+
+<script setup>
+const props = defineProps({
+   dataItem: {
+      type: Array,
+      required: true,
+   },
+});
+</script>
