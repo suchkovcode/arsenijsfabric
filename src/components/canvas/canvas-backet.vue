d2b0542c21f68dce67652878bbff00914e083952
Nikita Suchkov <suchkov8520@gmail.com>
2024-04-13 17:45:04 +0300

    Update backet camvas

--- src/components/canvas/canvas-backet.vue
+++ src/components/canvas/canvas-backet.vue
@@ -1,11 +1,9 @@
 <template>
    <div
       class="favorite fixed -right-full top-0 z-50 block h-screen w-full max-w-[450px] border-l-border bg-white transition-[right] duration-500 sm:border-l lg:max-w-[550px]"
-      :class="{ 'right-0 overflow-hidden': store.isActive }">
-      <canvas-header @close-canvas="store.updateStateActiveCanvas($event)">
-         Your order:
-      </canvas-header>
-      <div v-if="false" class="mb-5 flex h-full flex-col gap-5 overflow-y-scroll p-6 pb-32">
+      :class="{ 'right-0 overflow-hidden': store.isActiveBacket }">
+      <canvas-header @close-canvas="store.updateBacketCanvas($event)"> Your order: </canvas-header>
+      <div v-if="backet.cards.length" class="mb-5 flex h-full flex-col gap-5 overflow-y-scroll p-6 pb-32">
          <canvas-backet-item />
       </div>
       <p v-else class="m-6 rounded-xl bg-violet p-6 text-tertiary opacity-70">Your order list is empty</p>
@@ -13,5 +11,6 @@
 </template>
 
 <script setup>
-const store = useProductStore();
+const store = useAppStore();
+const backet = useBacketStore();
 </script>
